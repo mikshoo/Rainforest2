3c97991b38472c8384539b9ba5de30971f21f89a
mikshoo <mikshoo@gmail.com>
2014-02-05 15:38:50 +0100

    Adding authorization to ensure users are logged in before they create reviews

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,3 +11,11 @@ end
 
 helper_method :current_user
 end
+
+def ensure_logged_in
+    unless current_user
+        flash[:alert] = "Please log in"
+        redirect_to new_session_path
+    end
+end
+end
